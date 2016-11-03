971c9f9ec9beb052d78251cea64aea525836342d
Daniel Qiu <qsz1328@gmail.com>
2016-11-03 00:41:43 -0700

    pgg

--- MobilityModel/MobilityModel.py
+++ MobilityModel/MobilityModel.py
@@ -1,33 +1,33 @@
 """ Mobility Model  """
 
-
 import numpy as np
 from pykdtree.kdtree import KDTree
 import math
+
 from PGG.PGG import PGG
 
 PI = np.pi
 
 
 class MobilityModel:
-
     def __init__(self, N, mobile_map, lm_possibility, period):
         self.N = N
+        self.neighbor_limit = 30
         self.map = mobile_map
         self.period = period
         self.lm_possibility = lm_possibility
-        self.velocity = self.map.radius/period
+        self.velocity = self.map.radius / period
         self.neighbours = []
         self.pgg = PGG(N)
         self._init_points()
+        self._sigmoid = []
+        self._init_sigmoid()
 
+    def _init_sigmoid(self):
+        for i in xrange(self.neighbor_limit+1):
+            self._sigmoid.append(1 / (1 + math.exp(i - 10)))
 
-
-    @staticmethod
-    def _sigmoid(x):
-        return 1 / (1 + math.exp(x-10))
-
-    def _init_points(self ):
+    def _init_points(self):
         x = np.zeros(self.N)  # Cartesian Coordinate x
         y = np.zeros(self.N)  # Cartesian Coordinate y
         u_1 = np.random.uniform(0.0, 1.0, self.N)  # generate n uniformly distributed points
@@ -40,7 +40,7 @@ class MobilityModel:
         self.home_pos = zip(x, y)
         # print "init points:"+str(self.home_pos)
         self.cur_pos = self.home_pos
-        self.neighbours, self.neighbour_count = self._query_with_pykdtree(np.array(self.cur_pos))
+        self.neighbours, self.neighbour_count = self._query_with_pykdtree(np.array(self.cur_pos), k=self.neighbor_limit)
         # print "neighbours:" + str(self.neighbours)
 
     def one_day(self):
@@ -48,26 +48,29 @@ class MobilityModel:
         for i in xrange(self.period):
             goto_landmark = np.random.choice([0, 1], self.N, p=[1 - self.lm_possibility, self.lm_possibility])
             for idx, point in enumerate(self.cur_pos):
-                if goto_landmark[idx]: #goto landmark
+                if goto_landmark[idx]:  # goto landmark
                     lm = self.map.landmarks[landmark_selection[idx]]
                     a = np.arctan2(lm[1] - point[1], lm[0] - point[0])
                 else:  # random
                     a = 2 * np.pi * np.random.uniform(0.0, 1.0)
-                v = self.velocity * self._sigmoid(self.neighbour_count[idx])
+                v = self.velocity * self._sigmoid[self.neighbour_count[idx]]
                 self.cur_pos[idx] = (point[0] + v * np.cos(a), point[1] + v * np.sin(a))
-            self.neighbours, self.neighbour_count = self._query_with_pykdtree(np.array(self.cur_pos))
-
-            self.pgg.play(self.cur_pos, self.neighbours)
+            self.neighbours, self.neighbour_count = self._query_with_pykdtree(np.array(self.cur_pos),
+                                                                              k=self.neighbor_limit)
+            # print self.neighbours
+            self.pgg.play(self.neighbours, self.neighbour_count, resource = 1, enhancement = 3)
         #
         # test = self._query_with_pykdtree(np.array(self.cur_pos+self.map.landmarks))
         # print len(test[5000])
-        self._plot()
+        # self._plot()
         self.cur_pos = self.home_pos
 
-    def _query_with_pykdtree(self, points, k = 30, r = 1):
+    def _query_with_pykdtree(self, points, k=30, r=1):
         tree = KDTree(points)
-        return tree.query(points, k = k, distance_upper_bound = r)
-        #return [[x for x in nei if x < self.N] for nei in neibrs]
+        results, counts = tree.query(points, k=k, distance_upper_bound=r)
+
+        return [[n for n in r if n is not None] for r in results.tolist()], counts
+        # return [[x for x in nei if x < self.N] for nei in neibrs]
 
     def _plot(self):
         import matplotlib.pyplot as plt
@@ -84,5 +87,3 @@ class MobilityModel:
         plt.scatter(*zip(*self.map.landmarks), color='r')
 
         plt.show()
-
-
--- PGG/PGG.py
+++ PGG/PGG.py
@@ -2,12 +2,33 @@
 import numpy as np
 
 
+
 class PGG:
 
     def __init__(self, N):
         self.N = N
         self.strategy = np.random.randint(2, size=self.N)  # 1 as c 0 as d
 
-    # def play(self, points, neighbour):
+    def play(self, neighbour, neighbour_count, resource = 1, enhancement = 1):
+        pool = [0]*self.N
+        for idx, stgy in enumerate(self.strategy):
+            if stgy:
+                neibr_num = neighbour_count[idx]
+                contrib = float(resource)/neibr_num
+                for i in xrange(neibr_num):
+                    pool[neighbour[idx][i]] += contrib
+        profit = [0]*self.N
+        for idx, stgy in enumerate(pool):
+            neibr_num = neighbour_count[idx]
+            for i in xrange(neibr_num):
+                profit[neighbour[idx][i]] += (enhancement*pool[idx])/neibr_num
+
+        for idx, p in enumerate(profit):
+            if p > resource:
+                self.strategy[idx] = 1
+            else:
+                self.strategy[idx] = 0
+
+        # print profit
 
 
